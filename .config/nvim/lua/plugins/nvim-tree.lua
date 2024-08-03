c614d58876ef1c8b2f869f33b6511a52ac0297f9
chris <shonen787@gmail.com>
2024-08-02 20:45:28 -0700

    Initial Commit

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,44 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+		-- Setup Neotree with event handler to close on file open
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						-- Automatically close Neotree when a file is opened
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+				{
+					event = "neo_tree_buffer_enter",
+					handler = function()
+						vim.cmd("highlight! Cursor blend=100")
+					end,
+				},
+				{
+					event = "neo_tree_buffer_leave",
+					handler = function()
+						vim.cmd("highlight! Cursor guibg=#5f87af blend=0")
+					end,
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
+	end,
+}
---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.wo.relativenumber = true
+vim.opt.cursorline = true
+
+
+
